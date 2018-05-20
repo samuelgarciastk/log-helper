dd4451743b34450cbc92f3106d4e798a05d901f9
Samuel Garcia <samuelgarciastk@gmail.com>
2018-05-20 11:37:09 +0800

    v0.3.0

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -1,17 +1,23 @@
 import java.io.{File, PrintWriter}
+import java.util.concurrent.ConcurrentHashMap
+
+import scala.collection.mutable
+import scala.io.Source
 
 /**
   * Author: stk
   * Date: 18/4/6
   */
 object Parser {
+  val root = "F:\\GitHub Data\\"
+  val name = "100-hadoop"
+
   def main(args: Array[String]): Unit = {
-    val root = "C:\\Users\\stk\\Downloads\\test-data\\projects\\"
-    val name = "3-elasticsearch"
     val projectPath = root + name
     val outputPath = root + name + ".txt"
     val files = getBlameFiles(new File(projectPath)).toVector
-    generate(parseProject(files), outputPath)
+    //    generate(parseProject(files), outputPath)
+    sortProject(files)
   }
 
   def getBlameFiles(file: File): Array[File] = {
@@ -20,6 +26,34 @@ object Parser {
   }
 
   def parseProject(files: Vector[File]): Vector[String] = files.par.map(FileParser.parseFile).reduce(_ ++ _)
+
+  def sortProject(files: Vector[File]): Unit = {
+    val container = new ConcurrentHashMap[String, Int]()
+    val seq = files.par.map(sort).reduce((m1, m2) => m1 ++ m2.map { case (k, v) => k -> (v + m1.getOrElse(k, 0)) }).toSeq.sortBy(_._2).reverse
+    val writer = new PrintWriter(new File(root + name + "-loc.txt"))
+    seq.foreach(f => writer.write("%-10d\t%s\n".format(f._2, f._1)))
+    writer.flush()
+    writer.close()
+  }
+
+  def sort(file: File): mutable.HashMap[String, Int] = {
+    println("Sort file: " + file)
+    val regex = "^[\\^\\w]+\\s*(.*)\\s*\\((.*)\\s+\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} .\\d{4}\\s+(\\d+)\\) (.*)$".r
+    val source = Source.fromFile(file)
+    val lines = source.getLines
+    val map = new mutable.HashMap[String, Int]()
+    lines.foreach(f => {
+      regex.findFirstMatchIn(f).foreach(p => {
+        val name = p.group(2).trim
+        if (map.contains(name))
+          map += (name -> (map(name) + 1))
+        else
+          map += (name -> 1)
+      })
+    })
+    source.close()
+    map
+  }
 
   def generate(logs: Vector[String], path: String): Unit = {
     if (new File(path).delete) println("Delete existing result file.")
